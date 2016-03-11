180670cc143803f1d029debaebaecd63b9c89d04
thanh1101681 <thanh1101681@gmail.com>
2016-03-11 10:54:07 +0700

    recontructor

--- views/ReactDOMServer/browser.js
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = React.createFactory(require('./app'));
-ReactDOM.render(App(window.APP_PROPS), document.getElementById('content'));
